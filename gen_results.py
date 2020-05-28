282612ea689a61fd96fa233053bd4a4983b67285
Stefan Blattmann <stefan.blattmann@gmx.de>
2020-05-28 15:45:06 +0200

    Update gen_results.py
    update to scaled results

--- gen_results.py
+++ gen_results.py
@@ -11,7 +11,7 @@ data_set = data.parse("Spieltabelle2")
 #get first data tow 
 data_by_member = data_set.iloc[:88, 15:20]
 tmp_list = list()
-
+tmp_list_scaled = list()
 
 for i in range(0,88):
     tmp_data = data_by_member.iloc[i]
@@ -22,10 +22,13 @@ for i in range(0,88):
         if not np.isnan(num):
             tmp_cnt += 1
             tmp_sum += (1/num)
-    if tmp_sum is not 0.0:
-        tmp_list.append(tmp_sum/tmp_cnt)
+
+    value = tmp_sum/tmp_cnt
+    tmp_list.append(value)
+    tmp_list_scaled.append((1-value)*3)
 
 #build all together 
-result = data_by_member.assign(Result=tmp_list)
+result = data_by_member.assign(Result1=tmp_list)
+result = data_by_member.assign(Result_Scaled=tmp_list_scaled)
 
 result.to_excel("result.xlsx", sheet_name='Result')
